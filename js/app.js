b7b185597e4c0a38c39ea2acb619549c724e2862
hsyntes <hsyntes@gmail.com>
2022-11-29 07:01:48 +0200

    v3.1

--- js/app.js
+++ js/app.js
@@ -912,7 +912,10 @@ class App {
   _getPermission = () =>
     navigator.geolocation.getCurrentPosition(
       (position) => this._getWeatherData(position),
-      () => this._showError("You have to open location to use this app.")
+      () =>
+        this._showError(
+          "You have to open your location and allow it to use this app."
+        )
     );
 
   // Getting the current daily weather's data (clicked)
