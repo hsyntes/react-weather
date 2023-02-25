de04d990dd6f9f20be500e9d3a17e4f3382dc815
hsyntes <hsyntes@gmail.com>
2023-02-25 19:18:47 +0200

    fixing missed variable on Splash.js

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import logo from "../logo.svg";
 
-const Splash = ({ children }) => {
+const Splash = () => {
   const animationState = useSelector((state) => state.animation);
   const { splash, flicker } = animationState;
 
