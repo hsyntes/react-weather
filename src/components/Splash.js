995a7fa622f7d7359afc166a237b8ea367fb1cf6
hsyntes <hsyntes@gmail.com>
2023-04-03 02:39:43 +0300

    warning messages in Splash component

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,10 +3,21 @@ import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import logo from "../logo.svg";
+import { Toast, ToastContainer } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 const Splash = () => {
   const animationState = useSelector((state) => state.animation);
+  const [toast, setToast] = useState(false);
   const { splash, flicker } = animationState;
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setToast(true);
+    }, 3000);
+
+    return () => clearTimeout(identifier);
+  }, [toast]);
 
   return (
     <>
@@ -27,6 +38,17 @@ const Splash = () => {
           <FontAwesomeIcon icon={faSearch} className="ms-2" />
         </motion.p>
       </div>
+      <ToastContainer position="bottom-center" style={{ marginBottom: "5vh" }}>
+        <Toast show={toast} animation={true}>
+          <Toast.Header closeButton={false}>
+            <img src={logo} width={24} alt="App logo" />
+            <h4 className="ms-2">React-Weather</h4>
+          </Toast.Header>
+          <Toast.Body>
+            Please make sure that you have your location turned on.
+          </Toast.Body>
+        </Toast>
+      </ToastContainer>
     </>
   );
 };
