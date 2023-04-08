7a8a125597d021dd1e68299a49f41d9a42d8802c
hsyntes <hsyntes@gmail.com>
2023-04-08 03:48:22 +0300

    editing Toast message

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import logo from "../logo.svg";
-import { Toast, ToastContainer } from "react-bootstrap";
+import { Button, Card, Toast, ToastContainer } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
 const Splash = () => {
@@ -14,7 +14,7 @@ const Splash = () => {
   useEffect(() => {
     const identifier = setTimeout(() => {
       setToast(true);
-    }, 7500);
+    }, 3500);
 
     return () => clearTimeout(identifier);
   }, [toast]);
@@ -38,14 +38,24 @@ const Splash = () => {
           <FontAwesomeIcon icon={faSearch} className="ms-2" />
         </motion.p>
       </div>
-      <ToastContainer position="bottom-center" style={{ marginBottom: "5vh" }}>
+      <ToastContainer position="bottom-center" style={{ marginBottom: "10vh" }}>
         <Toast show={toast} animation={true}>
           <Toast.Header closeButton={false}>
             <img src={logo} width={24} alt="App logo" />
             <h4 className="ms-2">React-Weather</h4>
           </Toast.Header>
-          <Toast.Body>
-            Please make sure that your location turned on.
+          <Toast.Body className="d-flex flex-column">
+            <Card.Text className="mb-3">
+              Please make sure your device location is turned on.
+            </Card.Text>
+            <Button
+              type="button"
+              variant="primary"
+              className="p-0 py-2 px-4 ms-auto"
+              onClick={() => setToast(!toast)}
+            >
+              Got it
+            </Button>
           </Toast.Body>
         </Toast>
       </ToastContainer>
